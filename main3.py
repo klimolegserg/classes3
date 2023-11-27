30e37e491a802fab82b873793e2d401c9c7a4424
Oleg Klimenko <klimmail@rambler.ru>
2023-11-27 12:40:05 +0300

    initial commit

---
+++ main3.py
@@ -0,0 +1,83 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+        self.stud_midgrades = sum(self.grades.values)/len(self.grades)
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашнее задание: {self.stud_midgrades}\n\\
+              Курсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}'
+
+    def __eq__(self, other):
+        return self == other
+    
+    def __it__(self, other):
+        return self < other
+    
+    def __gt__(self, other):
+        return self > other   
+
+    def rate_lec(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.course_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+        
+           
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+    
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        self.courses_attached = []
+        self.grades = {}
+        self.lect_midgrades = sum(self.grades.values)/len(self.grades)
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.lect_midgrades}'
+    
+    def __eq__(self, other):
+        return self == other
+    
+    def __it__(self, other):
+        return self < other
+    
+    def __gt__(self, other):
+        return self > other
+  
+
+class Reviewer(Mentor):
+    def rate_nw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.course_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+        
+    def __str__(self):
+        return f'Имя: {self.name} Фамилия: {self.surname}'
+    
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
