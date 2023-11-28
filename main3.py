21a9787f1d5bdcf9b1ae927adf4c1cf977b2d064
Oleg Klimenko <klimmail@rambler.ru>
2023-11-28 13:10:15 +0300

    small changes

--- main3.py
+++ main3.py
@@ -6,11 +6,15 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        self.stud_midgrades = sum(self.grades.values)/len(self.grades)
+
+    def midgrade_st(self):
+        values_st = self.grades.values()
+        values_list = list(values_st)
+        average = sum(values_list)/len(values_list)
+        return average
 
     def __str__(self):
-        return f'Имя: {self.name}\n' f'Фамилия: {self.surname}\n' f'Средняя оценка за домашнее задание: {self.stud_midgrades()}\n\\
-            'f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n' f'Завершенные курсы: {", ".join(self.finished_courses)}'
+        return f'Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка за домашнее задание: {self.averege()}\n Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n Завершенные курсы: {", ".join(self.finished_courses)}'
 
     def __eq__(self, other):
         return self == other
@@ -22,7 +26,7 @@ class Student:
         return self > other   
 
     def rate_lec(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.course_attached:
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -40,12 +44,18 @@ class Mentor:
     
 class Lecturer(Mentor):
     def __init__(self, name, surname):
+        super().__init__(name, surname)
         self.courses_attached = []
         self.grades = {}
-        self.lect_midgrades = sum(self.grades.values)/len(self.grades)
+
+    def midgrade_st(self):
+        values_st = self.grades.values()
+        values_list = list(values_st)
+        average = sum(values_list)/len(values_list)
+        return average
 
     def __str__(self):
-        return f'Имя: {self.name}\n' f'Фамилия: {self.surname}\n' f'Средняя оценка за лекции: {self.lect_midgrades()}'
+        return f'Имя: {self.name}\n' f'Фамилия: {self.surname}\n' f'Средняя оценка за лекции: {self.average()}'
     
     def __eq__(self, other):
         return self == other
@@ -58,8 +68,12 @@ class Lecturer(Mentor):
   
 
 class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+        
     def rate_nw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.course_attached and course in student.courses_in_progress:
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
